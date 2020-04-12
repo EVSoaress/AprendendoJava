90df9cf9759de7eb4a2134083a0fdec8d018009b
Erick Soares <erickfreela@gmail.com>
2020-04-11 22:56:11 -0300

    Iniciando

---
+++ estrutura-condicional/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner er = new Scanner(System.in);
+//		int hora;
+//		
+//		hora = er.nextInt();
+//		
+//		if(hora <= 12 && hora >= 5) {
+//			System.out.println("Bom Dia");
+//		} 
+//		else{
+//			System.out.println("Boa noite");
+//			
+//		}
+		
+//		double preco;
+//		preco = er.nextDouble();
+//		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+//		System.out.println(desconto);
+//		
+		
+	}
+
+}
---
+++ teste-processamento/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		double a;
+		int b;
+		
+		a = 5.5875698;
+		b = (int) a;
+		
+		System.out.println(b);
+		
+		
+	}
+
+}
