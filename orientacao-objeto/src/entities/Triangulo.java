35f7aeecfd8b4d6edc7253ae048d0b35d5681367
Erick Soares <erickfreela@gmail.com>
2020-04-14 16:23:55 -0300

    melhoras na orientação a objetos(deixando o codigo mais limpo)

--- orientacao-objeto/src/entities/Triangulo.java
+++ orientacao-objeto/src/entities/Triangulo.java
@@ -5,5 +5,11 @@ public class Triangulo {
 	public double a;
 	public double b;
 	public double c;
+	
+	public double area() {
+	
+		double p = (a + b + c);
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 
 }
